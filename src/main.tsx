42a403d4736dc525c7f1c87329699ac8c61c6a75
Sumit Mahat <mahatsumit5@gmail.com>
2024-07-09 14:47:36 +1000

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Auth0Provider } from "@auth0/auth0-react";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-fkp34f1yfajuoqj7.au.auth0.com"
-      clientId="OeRGbMoN3lzzyuI9hfbKqZXhjGc3lfMc"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </React.StrictMode>
 );
